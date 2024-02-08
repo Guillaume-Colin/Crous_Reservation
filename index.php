5daa6f23899923a2a458c0dad2c58823919c5929
Guillaume COLIN <guillaume.colin@etu.univ-nantes.fr>
2024-02-08 10:41:23 +0100

    index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CROUS</title>
+</head>
+<body>
+    Accueil
+    <?php
+
+    // require "./env.php";
+    // require "./inc/config.php";
+    // $dotenv = Dotenv\Dotenv::createImmutable(__DIR__/"/../");
+    // $dotenv-> load();
+
+    ?>
+    
+</body>
+</html>
+
+
+
