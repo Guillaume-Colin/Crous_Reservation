ff3b2fe5b066885c8f7580c84e4d4cf59d86eee4
Guillaume COLIN <guillaume.colin@etu.univ-nantes.fr>
2024-02-12 23:44:28 +0100

    accueil.php

--- accueil.php
+++ accueil.php
@@ -3,57 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Navbar Example</title>
-    <style>
-        body {
-            margin: 0;
-            font-family: Arial, sans-serif;
-        }
-
-        nav {
-            background-color: #333;
-            color: #fff;
-            display: flex;
-            align-items: center;
-            padding: 10px 20px;
-        }
-
-        .logo img {
-            width: 50px;
-        }
-
-        .nav-links {
-            flex-grow: 1;
-            text-align: center;
-        }
-
-        .nav-links a {
-            color: #fff;
-            text-decoration: none;
-            margin: 0 10px;
-        }
-
-        .user-info {
-            display: flex;
-            align-items: center;
-        }
-
-        .user-info img {
-            width: 30px;
-            margin-left: 10px;
-            border-radius: 50%;
-        }
-    </style>
+    <title>Accueil</title>
 </head>
 <body>
     <?php
-function view($name)
+    
+function view2($name)
 {
-    include("./views/$name.view.php");
+    include("./src/Views/$name.view.php");
 }
-view("navbar");
+require 'src/Views/navbar.view.php';
+
+view2('horaire');
+
 ?>
-
-
 </body>
 </html>
