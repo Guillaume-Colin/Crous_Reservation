6665688daea1e255abf03ac9190ba1c80e44ec07
Guillaume COLIN <guillaume.colin@etu.univ-nantes.fr>
2024-02-18 21:53:20 +0100

    acceuil.php

--- accueil.php
+++ accueil.php
@@ -7,18 +7,18 @@
 </head>
 <body>
     <?php
-
     require_once './inc/functions_db.php';
     require_once './inc/functions.php';
-    session_start();
-    $_SESSION['idPersonne']='E223029G';
-
+    require_once('./inc/config.php');
+    init_php_session();
+    ensure_user_is_authentificated();
+    
     if(isset($_GET['idToDelete']))
     {
         $idResto = explode("|",  $_GET['idToDelete'])[0];
         $dateReserve = explode("|",  $_GET['idToDelete'])[1];
         exec_request('DELETE FROM RESERVE 
-        WHERE id_personne = \''.$_SESSION['idPersonne'].'\'
+        WHERE id_personne = \''.$_SESSION['login'].'\'
         AND id_restoCrous = \''.$idResto.'\'
         AND date_reserve = \''.$dateReserve.'\''
         );
@@ -28,7 +28,7 @@
     $tabReservations = exec_request('SELECT R.id_restoCrous, id_personne, date_reserve, deuxieme_reserve, nom_resto
         FROM RESERVE R
         JOIN RESTO_CROUS RC ON (R.id_restoCrous = RC.id_restoCrous) 
-        WHERE id_personne = \''.$_SESSION['idPersonne'].'\'
+        WHERE id_personne = \''.$_SESSION['login'].'\'
         ORDER BY date_reserve DESC'
         );
 
