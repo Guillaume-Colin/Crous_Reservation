1688c7c8b671ebfa4a61d4e0af9ec79ba48eea42
Guillaume COLIN <guillaume.colin@etu.univ-nantes.fr>
2024-02-18 19:03:53 +0100

    menu.php

---
+++ menu.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Choix Menu</title>
+</head>
+<body>
+    <?php
+    require_once './inc/functions_db.php';
+    require_once './inc/functions.php';
+    require_once('./inc/config.php');
+
+    session_start();
+
+    $idEntree = null;
+    $idPlat = null;
+    $idDessert = null;
+    
+    $controle = "N";
+    if(isset($_POST['controleMenu']))
+    {
+        $controle = $_POST['controleMenu'];
+    } 
+    
+    if ($controle=="O")
+    {
+        $idEntree = $_POST['entree'];
+        $idPlat = $_POST['plat'];
+        $idDessert = $_POST['dessert'];
+        if ($idEntree!='' || $idPlat!='' || $idDessert!='')
+        {        
+            $resultat = exec_request('INSERT INTO RESERVE 
+                VALUES('.$_SESSION['idResto'].', 
+                       \''.$_SESSION['idPersonne'].'\', 
+                       \''.$_SESSION['date'].'\', 
+                        0, 
+                        \''.$_SESSION['plageHoraire'].'\', 
+                        '.$idEntree.',
+                        '.$idPlat.', 
+                        '.$idDessert.')');
+            redirect('./Accueil.php');
+        }
+    }
+
+    $listeEntrees = exec_request('SELECT id_article, nom_article FROM article JOIN menu on (nom_article = entree) order by nom_article asc');
+    $listePlats = exec_request('SELECT id_article, nom_article FROM article JOIN menu on (nom_article = plat_principal) order by nom_article asc');
+    $listeDesserts = exec_request('SELECT id_article, nom_article FROM article JOIN menu on (nom_article = dessert) order by nom_article asc');
+
+    $tabParam = array(
+        'listeEntrees' => $listeEntrees,
+        'listePlats' => $listePlats,
+        'listeDesserts' => $listeDesserts,
+        'idEntree' => $idEntree,
+        'idPlat' => $idPlat,
+        'idDessert' => $idDessert
+    );
+    view('menu',$tabParam);?>
+</body>
+</html>
