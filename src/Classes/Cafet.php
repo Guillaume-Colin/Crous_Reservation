7a853738309fb4778cc0be18ad97992761de19fe
Guillaume COLIN <guillaume.colin@etu.univ-nantes.fr>
2024-02-18 19:47:42 +0100

    src

--- src/Classes/Cafet.php
+++ src/Classes/Cafet.php
@@ -1,2 +1,26 @@
 //HERITAGE RESTO CROUS//
 
+<?php
+
+/**
+ * Class Cafet
+ */
+class Cafet extends Resto_Crous 
+
+{
+    private ;
+    private ;
+
+
+    public function __construct($Id_restocrous, $nb_place)
+    {
+        $this->Id_restocrous = $Id_restocrous;
+        $this->nb_place = $nb_place;
+        parent::__construct();
+    }
+
+}
+
+8====D
+
+un zgueg ^^
